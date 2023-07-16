89fcb7af34985b9bd114c6731700055e1cc28488
lz <ludizh@gmail.com>
2023-07-16 15:53:48 -0700

    Add onclick event to the button

---
+++ index.js
@@ -0,0 +1,6 @@
+let inputBtn = document.getElementById('input-btn');
+
+// Another way to handle onclick event
+inputBtn.addEventListener('click', function () {
+  console.log('Button clicked!');
+});
